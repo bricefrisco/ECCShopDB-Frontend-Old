6d65315fd7b63021a6e1ad68603b3046c26c3b17
bricefrisco <bricemfrisco@gmail.com>
2021-01-14 09:50:22 -0600

    Add alert notification letting users know about update

--- src/Search/ChestShops/Results/ChestShopSignResults/ChestShopSignResults.js
+++ src/Search/ChestShops/Results/ChestShopSignResults/ChestShopSignResults.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Alert from '@material-ui/lab/Alert';
+import IconButton from '@material-ui/core/IconButton';
+import Collapse from '@material-ui/core/Collapse';
+import Button from '@material-ui/core/Button';
+import CloseIcon from '@material-ui/icons/Close';
 
 import TopPanel from '../TopPanel/TopPanel';
 import ChestShopSigns from './ChestShopSigns';
@@ -20,8 +25,39 @@ const ChestShopSignResults = ({
 }) => {
   const classes = useStyles();
 
+  const [alertOpen, setAlertOpen] = React.useState(true);
+
   return (
     <div className={classes.searchResults}>
+      <Collapse in={alertOpen}>
+        <Alert
+          severity='info'
+          action={
+            <IconButton
+              aria-label='close'
+              color='inherit'
+              size='small'
+              onClick={() => {
+                setAlertOpen(false);
+              }}
+            >
+              <CloseIcon fontSize='inherit' />
+            </IconButton>
+          }
+        >
+          All chest shops have been cleared after an update. Please read{' '}
+          <a
+            href='https://ecocitycraft.com/forum/threads/beta-ecc-shopdb-search-find-and-update-shop-items.192909/page-9#post-1107111'
+            className='alertLink'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            the forum post
+          </a>{' '}
+          for more info!
+        </Alert>
+      </Collapse>
+
       <TopPanel
         loading={loading}
         pagination={pagination}
